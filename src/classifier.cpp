b4bcaa920ae7d58a38a540269be4c98277d1341d
Joseph Furiate <furiatejoseph@yahoo.com>
2016-04-02 22:23:49 -0400

    Clean up

--- src/classifier.cpp
+++ src/classifier.cpp
@@ -14,49 +14,9 @@ NBC::NBC(const string& trainingFileName, const string& testingFileName) {
 }	 	
 
 void NBC::train(const string& fileName) {
-	cout << "NBC train called\n";
 	// Store training set data into memory
 	readFile(fileName, this->trainingSet);
 	classifier(this->trainingSet);
-	
-	// calc probabilities
-	/*
-	for(auto it = trainingSet.begin(); it != trainingSet.end(); ++it) {
-		if(means.find(it->first) == means.end())
-			means.insert( make_pair(it->first, vector<double>(max_length, 0.0) ));
-
-		size_t tmp = 0;
-		for(auto& count : means.find(it->first)->second)
-			if(tmp < it->second.size())
-				count += it->second.at(tmp++);
-	}
-
-	// Print means for each class
-	cout << "key\tvalues\n";
-	for(auto it = means.cbegin(); it != means.end(); ++it) {
-		cout << it->first << '\t';
-		for(const auto& j : it->second) 
-			cout << j << ' ';
-		cout << '\n';
-		
-	}
-
-	size_t count_1 = trainingSet.count(-1);
-	size_t count1 = trainingSet.count(1);
-	for(auto it = means.begin(); it != means.end(); ++it) {
-		for(auto& j : it->second)
-			j /= trainingSet.count(it->first);
-	}
-	
-	cout << "(attr_count) / total\n";
-	cout << "count-1: " << count_1 << "\tcount1: " << count1 << '\n';
-	cout << "key\tvalues\n";
-	for(auto it = means.cbegin(); it != means.end(); ++it) {
-		cout << it->first << '\t';
-		for(const auto& j : it->second)
-			cout << j << ' ';
-		cout << '\n';
-	} */
 }
 
 void NBC::test(const string& fileName) {
@@ -66,80 +26,79 @@ void NBC::test(const string& fileName) {
 
 // Classifies data file
 void NBC::classifier(multimap<int, vector<int> >& myMap ) {
-	cout << "NBC test called\n";
-
-	// Store testing set data into memory
+	// Probability stuff
 	int negativeCount = myMap.count(-1);
 	int positiveCount = myMap.count(1);
 	double total = negativeCount + positiveCount;
 	double prob_positive_class = myMap.count(1) / total;
 	double prob_negative_class = myMap.count(-1) / total;
-
-	cout << "prob(1): " << prob_positive_class << '\t'
-		 << "prob(-1): " << prob_negative_class << '\n';
-
-	cout << "max_length: " << max_length << '\n';
-
 	int accuracy = 0;
 	int truePositives = 0, falsePositives = 0,
 	   	trueNegatives = 0, falseNegatives = 0;
 
-	for(auto current = myMap.cbegin();
-		   	current != myMap.cend(); ++current)  {
-	const auto& values = current->second;
-	multimap<int, vector<double> > prob;
-	int currentLabel = current->first;
 
-	for(size_t attr = 0; attr < values.size(); ++attr) {
-		// find attr given class
-		for(auto set = myMap.begin(); set != myMap.end(); ++set) {
-			int setLabel = set->first;
-			// Add new key for unencountered class
-			if(prob.find(setLabel) == prob.end())
-				prob.insert(make_pair(setLabel, vector<double>(max_length, 0)));
+	cout << "\n\nprob(-1): " << prob_negative_class 
+		 << "\ttotal(-1): " << negativeCount << '\n'
+		 << "prob(1): " << prob_positive_class
+		 << "\ttotal(1): " << positiveCount << "\n\n";
 
-			vector<double> * ptr = &prob.find(setLabel)->second;
+	//cout << "max_length: " << max_length << '\n';
 
-			if(attr < set->second.size()
-				   	&& values.at(attr) == set->second.at(attr))
-				ptr->at(attr) += 1;
+	for(auto current = myMap.cbegin(); current != myMap.cend(); ++current)  {
+		const auto& values = current->second;
+		multimap<int, vector<double> > prob;
+		int currentLabel = current->first;
+
+		for(size_t attr = 0; attr < values.size(); ++attr) {
+			// find attr given class
+			for(auto set = myMap.begin(); set != myMap.end(); ++set) {
+				int setLabel = set->first;
+				// Add new key for unencountered class
+				if(prob.find(setLabel) == prob.end())
+					prob.insert(make_pair(setLabel, vector<double>(max_length, 1)));
+
+				vector<double> * ptr = &prob.find(setLabel)->second;
+
+				if(attr < set->second.size()
+						&& values.at(attr) == set->second.at(attr))
+					ptr->at(attr) += 1;
+			}
 		}
-	}
+			
+		for(size_t attr = 0; attr < max_length; ++attr) {
+			prob.find(-1)->second.at(attr) /= negativeCount;
+			prob.find(1)->second.at(attr) /= positiveCount;
+		}
+
 		
-	for(size_t attr = 0; attr < max_length; ++attr) {
-		prob.find(-1)->second.at(attr) /= negativeCount;
-		prob.find(1)->second.at(attr) /= positiveCount;
-	}
+		double pb_neg = prob_negative_class;
+		double pb_pos = prob_positive_class;
+		for(size_t attr = 0; attr < max_length; ++attr) {
+			pb_neg *= prob.find(-1)->second.at(attr);
+			pb_pos *= prob.find(1)->second.at(attr);
+		}	
 
-	
-	double pb_neg = prob_negative_class;
-	double pb_pos = prob_positive_class;
-	for(size_t attr = 0; attr < max_length; ++attr) {
-		pb_neg *= prob.find(-1)->second.at(attr);
-		pb_pos *= prob.find(1)->second.at(attr);
-	}	
+	/*
+		cout << "pb_neg: " << pb_neg << "\tpb_pos: " << pb_pos << '\n';
+		cout << "test counts\n";
+		for(auto it = prob.cbegin(); it != prob.end(); ++it) {
+			cout << it->first << '\t';
+			for(const auto& j : it->second) 
+				cout << j << ' ';
+			cout << '\n';
+		}
+	*/
+		// Calculate the true/false positives/negatives
+		int choice = pb_neg > pb_pos ? -1 : 1;
+	//	cout << "True: " << currentLabel << "   Guess: " << choice
+	//		 << "\t" << (currentLabel == choice ? "r" : "w") << '\n';
+		if(currentLabel == choice) accuracy++;
+		if(currentLabel == 1 && choice == 1) truePositives++;
+		if(currentLabel == -1 && choice == 1) falsePositives++;
+		if(currentLabel == -1 && choice == -1) trueNegatives++;
+		if(currentLabel == 1 && choice == -1) falseNegatives++;
+		} // end big for loop
 
-/*
-	cout << "pb_neg: " << pb_neg << "\tpb_pos: " << pb_pos << '\n';
-	cout << "test counts\n";
-	for(auto it = prob.cbegin(); it != prob.end(); ++it) {
-		cout << it->first << '\t';
-		for(const auto& j : it->second) 
-			cout << j << ' ';
-		cout << '\n';
-	}
-*/
-	// Calculate the true/false positives/negatives
-	int choice = pb_neg > pb_pos ? -1 : 1;
-//	cout << "True: " << currentLabel << "   Guess: " << choice
-//		 << "\t" << (currentLabel == choice ? "r" : "w") << '\n';
-	if(currentLabel == choice)
-		accuracy++;
-	if(currentLabel == 1 && choice == 1) truePositives++;
-	if(currentLabel == -1 && choice == 1) falsePositives++;
-	if(currentLabel == -1 && choice == -1) trueNegatives++;
-	if(currentLabel == 1 && choice == -1) falseNegatives++;
-	} // end big for loop
 	cout << "accuracy: " << accuracy/total << '\n';
 	cout << "True Positives: " << truePositives << '\n'
 		 << "False Positives: " << falsePositives << '\n'
@@ -148,7 +107,6 @@ void NBC::classifier(multimap<int, vector<int> >& myMap ) {
 }	
 
 void NBC::readFile(const string& fileName, multimap<int, vector<int> >& myMap) { 
-	cout << "NBC readFile called\n";
 	ifstream inFile(fileName);
 
 	if(!inFile) {
@@ -207,14 +165,45 @@ void NBC::readFile(const string& fileName, multimap<int, vector<int> >& myMap) {
 		if(values.size() > max_length) max_length = values.size();
 		
 		myMap.insert(make_pair(label, values));
-/*
-		cout << label << '\t';
-		for(const auto& i : *values)
-			cout << i << ' ';
-		cout << '\n';
-*/
 	} // end of outer while
 
 	inFile.close();
 }
+/* 
+	for(auto it = trainingSet.begin(); it != trainingSet.end(); ++it) {
+		if(means.find(it->first) == means.end())
+			means.insert( make_pair(it->first, vector<double>(max_length, 0.0) ));
+
+		size_t tmp = 0;
+		for(auto& count : means.find(it->first)->second)
+			if(tmp < it->second.size())
+				count += it->second.at(tmp++);
+	}
+
+	// Print means for each class
+	cout << "key\tvalues\n";
+	for(auto it = means.cbegin(); it != means.end(); ++it) {
+		cout << it->first << '\t';
+		for(const auto& j : it->second) 
+			cout << j << ' ';
+		cout << '\n';
+		
+	}
+
+	size_t count_1 = trainingSet.count(-1);
+	size_t count1 = trainingSet.count(1);
+	for(auto it = means.begin(); it != means.end(); ++it) {
+		for(auto& j : it->second)
+			j /= trainingSet.count(it->first);
+	}
+	
+	cout << "(attr_count) / total\n";
+	cout << "count-1: " << count_1 << "\tcount1: " << count1 << '\n';
+	cout << "key\tvalues\n";
+	for(auto it = means.cbegin(); it != means.end(); ++it) {
+		cout << it->first << '\t';
+		for(const auto& j : it->second)
+			cout << j << ' ';
+		cout << '\n';
+	} */
 
